09a965375334f0dfd8090ba86adf422a22fdb732
Danilo Karol Marijanac <dmarijan@cbr13s5.42barcelona.com>
2025-02-27 15:22:11 +0100

    LETS GOOOOOOOOOOOOOOOOOOOOOOOO

--- src/xrayzzzz.c
+++ src/xrayzzzz.c
@@ -6,7 +6,7 @@
 /*   By: dmarijan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 18:48:56 by dmarijan          #+#    #+#             */
-/*   Updated: 2025/02/22 19:29:28 by dmarijan         ###   LAUSANNE.ch       */
+/*   Updated: 2025/02/27 15:19:43 by dmarijan         ###   LAUSANNE.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -217,160 +217,22 @@ float	ft_round(float nbr)
 	return (whole + (nbr / 10));
 }
 
-void	comp_upwards(t_square *sq, float *ogx, float *ogy, float step)
-{
-	float	slope;
-	float	tempy;
-	float	tempx;
-
-	slope = tanf(dtr(sq->angle));
-	tempy = (*ogy) - absf(slope * step);
-	tempx = *ogx;
-	if (truncf(tempy) != truncf(*ogy))
-	{
-		if (sq->angle <= 90 || sq->angle >= 270)
-			tempx -= (*ogy - truncf(*ogy)) / absf(slope);
-		else
-			tempx += (*ogy - truncf(*ogy)) / absf(slope);
-		*ogy = truncf(*ogy);
-		*ogx = tempx;
-	}
-	else
-		*ogy = tempy;
-	printf("ogx: %f ogy: %f newy: %f slope: %f, step: %f\n", *ogx, *ogy, tempy, slope, step);
-}
-
-void	comp_downwards(t_square *sq, float *ogx, float *ogy, float step)
-{
-	float	slope;
-	float	tempy;
-	float	tempx;
-
-	slope = tanf(dtr(sq->angle));
-	tempy = (*ogy) + absf(slope * step);
-	tempx = *ogx;
-	if (truncf(tempy) != truncf(*ogy))
-	{
-		if (sq->angle <= 90 || sq->angle >= 270)
-			tempx -= (truncf(tempy) - *ogy) / absf(slope);
-		else
-			tempx += (truncf(tempy) - *ogy) / absf(slope);
-		*ogy = truncf(tempy);
-		*ogx = tempx;
-	}
-	else
-		*ogy = tempy;
-//	printf("ogx: %f ogy: %f newx: %f newy: %f slope: %f, step: %f\n", *ogx, *ogy, tempx, tempy, slope, step);
-}
-
-void	neo_move_x(t_square *sq, float *ogx, float *ogy)
-{
-	float	step;
-
-	if (sq->angle <= 90 || sq->angle >= 270)
-		step = truncf((*ogx) + 1) - (*ogx);
-	else
-		step = (*ogx - truncf(*ogx + 1));
-	printf("im neo moving X: step: %f, ogx: %f, ogy %f\n", step, *ogx, *ogy);
-	*ogx += step;
-	if (sq->angle >= 0 && sq->angle <= 180)
-		comp_upwards(sq, ogx, ogy, step);
-	else if (sq->angle > 180 && sq->angle < 360)
-		comp_downwards(sq, ogx, ogy, step);
-	printf("im neo moving X AFTER MOVE: step: %f, ogx: %f, ogy %f\n\n", step, *ogx, *ogy);
-}
-
-void	comp_right(t_square *sq, float *ogx, float *ogy, float step)
-{
-	float	slope;
-	float	tempy;
-	float	tempx;
-
-	if (sq->angle == 90 || sq->angle == 270)
-		slope = 0;
-	else
-		slope = 1 / tanf(dtr(sq->angle));
-	tempx = (*ogx) + absf(slope * step);
-	tempy = *ogy;
-	if (truncf(tempx) != truncf(*ogx))
-	{
-		if (sq->angle >= 180 && sq->angle <= 0)
-			tempy += (truncf(tempx) - *ogx) / absf(slope);
-		else
-			tempy -= (truncf(tempx) - *ogx) / absf(slope);
-		*ogy = tempy;
-		*ogx = truncf(tempx);
-	}
-	else
-		*ogy = tempy;
-	printf("ogx: %f ogy: %f newx: %f newy: %f slope: %f, step: %f\n", *ogx, *ogy, tempx, tempy, slope, step);
-}
-
-void	comp_left(t_square *sq, float *ogx, float *ogy, float step)
-{
-	float	slope;
-	float	tempy;
-	float	tempx;
-
-	if (sq->angle == 90 || sq->angle == 270)
-		slope = 0;
-	else
-		slope = 1 / tanf(dtr(sq->angle));
-	tempx = (*ogx) - absf(slope * step);
-	tempy = *ogy;
-	if (truncf(tempx) != truncf(*ogx))
-	{
-		if (sq->angle >= 180 && sq->angle <= 0)
-			tempy += (truncf(tempx) - *ogx) / absf(slope);
-		else
-			tempy -= (truncf(tempx) - *ogx) / absf(slope);
-		*ogy = tempy;
-		*ogx = truncf(*ogx);
-	}
-	else
-		*ogy = tempy;
-	printf("ogx: %f ogy: %f newx: %f newy: %f slope: %f, step: %f\n", *ogx, *ogy, tempx, tempy, slope, step);
-}
-
-void	neo_move_y(t_square *sq, float *ogx, float *ogy)
-{
-	float	step;
-
-	printf("im neo moving Y\n");
-	if ((sq->angle >= 0 && sq->angle <= 180))
-		step = (*ogy - truncf(*ogy) + 1);
-	else
-		step = (truncf(*ogy + 1) - *ogy);
-	*ogy += step;
-	if (sq->angle < 90 || sq->angle > 270)
-		comp_right(sq, ogx, ogy, step);
-	else if (sq->angle > 90 && sq->angle < 270)
-		comp_left(sq, ogx, ogy, step);
-}
-
 void	ft_bts(t_square *sq, float x, float y, float i)
 {
-	float	decx;
-	float	decy;
-
-	decx = x / roundf(x);
-	decy = y / roundf(y);
-	if (absf(x - roundf(x)) < absf(y - roundf(y)))
+	if (x == (int)x)
 	{
-		if (decx > 1)
-			sq->bts[(int)i] = EAST;
-		if (decx < 1)
+		if (sq->angle > 90 && sq->angle < 270)
 			sq->bts[(int)i] = WEST;
+		else
+			sq->bts[(int)i] = EAST;
 	}
-	else if (absf(x - roundf(x)) > absf(y - roundf(y)))
+	else if (y == (int)y)
 	{
-		if (decy > 1)
-			sq->bts[(int)i] = SOUTH;
-		if (decy < 1)
+		if (sq->angle > 0 && sq->angle < 180)
 			sq->bts[(int)i] = NORTH;
+		else
+			sq->bts[(int)i] = SOUTH;
 	}
-	else
-		sq->bts[(int)i] = NONE;
 }
 
 float	ft_roundingit(t_square *sq, float *x, float *y, float i)
@@ -397,10 +259,7 @@ float	ft_roundingit(t_square *sq, float *x, float *y, float i)
 	if (sq->map[(int)*y][(int)*x] == '1')
 		distance = (sqrt(abspwr(absf(*x) - sq->pcoord.x) + abspwr(absf(*y) - sq->pcoord.y)));
 	if (distance)
-	{
 		ft_bts(sq, *x, *y, i);
-		printf("x coord y coord: %f %f\n\n", *x, *y);
-	}
 	return (distance);
 }
 
@@ -410,9 +269,9 @@ void	fix_x(t_square *sq, float *x, float *y, float slope)
 
 	tx = *x;
 	if (sq->angle <= 90 || sq->angle >= 270)
-		tx = (ceilf(*x));
+		tx = (ceilf(*x + 0.001));
 	else
-		tx = (floorf(*x));
+		tx = (floorf(*x - 0.001));
 	if (sq->angle >= 0 && sq->angle <= 180)
 		*y -= absf((tx - *x) * slope);
 	else
@@ -426,28 +285,26 @@ void	fix_y(t_square *sq, float *x, float *y, float slope)
 
 	ty = *y;
 	if (sq->angle >= 0 && sq->angle <= 180)
-		ty = floorf(*y);
+		ty = floorf(*y - 0.001);
 	else
-		ty = ceilf(*y);
+		ty = ceilf(*y + 0.001);
 	if (sq->angle <= 90 || sq->angle >= 270)
 		*x += absf((ty - *y) / slope);
 	else
 		*x -= absf((ty - *y) / slope);
 	*y = ty;
-	printf("handle y, \n");
 }
 
 void	handle_pope(t_square *sq, float *x, float *y)
 {
-	printf("handle pope\n");
 	if (sq->angle == 0)
-		*x = (ceilf(*x + 0.001));
+		*x = (ceilf(*x + 0.01));
 	else if (sq->angle == 180)
-		*x = (floorf(*x - 0.001));
+		*x = (floorf(*x - 0.01));
 	if (sq->angle == 270)
-		*y = (ceilf(*y + 0.001));
+		*y = (ceilf(*y + 0.01));
 	else if (sq->angle == 90)
-		*y = (floorf(*y - 0.001));
+		*y = (floorf(*y - 0.01));
 }
 
 void	movement(t_square *sq, float *x, float *y, float slope)
@@ -460,22 +317,21 @@ void	movement(t_square *sq, float *x, float *y, float slope)
 	else
 	{
 		if (sq->angle <= 90 || sq->angle >= 270)
-			rdx = (ceilf(*x + 0.0001) - *x) * slope;
+			rdx = absf((ceilf(*x + 0.001) - *x));
 		else
-			rdx = (*x - floorf(*x - 0.0001)) * slope;
+			rdx = absf((*x - floorf(*x - 0.001)));
 		if (sq->angle >= 0 && sq->angle <= 180)
-			rdy = (*y - floorf(*y - 0.0001)) / slope;
+			rdy = absf((*y - floorf(*y - 0.001)) / slope);
 		else
-			rdy = (ceilf(*y + 0.0001) - *y) / slope;
+			rdy = absf((ceilf(*y + 0.001) - *y) / slope);
 		if (rdx <= rdy)
 			fix_x(sq, x, y, slope);
 		else
 			fix_y(sq, x, y, slope);
 	}
-	printf("coords: x: %f, y: %f, slope: %f\n", *x, *y, slope);
 }
 
-float	zeus(t_square *sq)
+float	zeus(t_square *sq, int i)
 {
 	float	slope;
 	float	x;
@@ -490,8 +346,27 @@ float	zeus(t_square *sq)
 	while (!gonetoofar(sq->pcoord, x, y))
 	{
 		movement(sq, &x, &y, slope);
+		if (y == (int)y && sq->angle > 0 && sq->angle < 180)
+		{
+			if (y - 1 >= 0 && sq->map[(int)y - 1][(int)x] == '1')
+			{
+				ft_bts(sq, x, y, i);
+				return (sqrt(abspwr(absf(x) - sq->pcoord.x) + abspwr(absf(y) - sq->pcoord.y)));
+			}
+		}
+		if (x == (int)x && sq->angle > 90 && sq->angle < 270)
+		{
+			if (x - 1 >= 0 && sq->map[(int)y][(int)x - 1] == '1')
+			{
+				ft_bts(sq, x, y, i);
+				return (sqrt(abspwr(absf(x) - sq->pcoord.x) + abspwr(absf(y) - sq->pcoord.y)));
+			}
+		}
 		if (sq->map[(int)y][(int)x] == '1')
+		{
+			ft_bts(sq, x, y, i);
 			return (sqrt(abspwr(absf(x) - sq->pcoord.x) + abspwr(absf(y) - sq->pcoord.y)));
+		}
 	}
 	return (-1);
 }
@@ -510,8 +385,7 @@ void	coneheads(t_square *sq)
 		else if (angle >= 360)
 			angle -= 360;
 		sq->angle = angle;
-		printf("CALLING ZEUS WITH ANGLE %f\n", angle);
-		sq->cone[(int)i] = zeus(sq);
+		sq->cone[(int)i] = zeus(sq, i);
 		i++;
 	}
 }
